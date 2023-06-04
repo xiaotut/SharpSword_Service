1a385f7f7a27478f6e4789908a601fae54a8413c
xiaotut <xdzhaoyuheng@163.com>
2023-06-04 12:18:06 +0800

    first commit

---
+++ src/main/java/org/zyh/boot/MainApplication.java
@@ -0,0 +1,16 @@
+package org.zyh.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther : Zhao Yuheng
+ * @Description : 启动springBoot项目的入口
+ */
+@SpringBootApplication // 这个注解明确的指出这是一个springBoot项目
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
